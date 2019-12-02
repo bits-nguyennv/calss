caa4681edf5abc3be5d49d4908d1c1fdfb827fb9
nguyenvannguyen <tieulongnuhd9x@gmail.com>
2019-12-02 17:21:55 +0900

    them

--- 9.php
+++ 9.php
@@ -19,46 +19,68 @@ class dbconnect {
         }else{
             echo "kết nối thành công";   
         } 
+
     }       
     function insert($table,$colum,$values){
         $sql = "INSERT INTO $table ($colum) VALUES ($values);";
         $insert = $this->conn->query($sql);
         if ($insert == true){
+            echo " thành công";
+        } else {
+            echo " k thành công".$this->conn->error;
+        }   
+    }
+    // function select($table){
+    //     $sql = "SELECT * FROM $table";
+    //     $select = $this->conn->query($sql);
+    //    $rows =  mysqli_fetch_all($select);
+    //     if ($select == true){
+    //             echo " thành công";
+    //         } else {
+    //             echo " k thành công".$this->conn->error;
+    //         } 
+    //         return $rows;  
+    // }
+
+    function insert1($table1,$colum1,$values1){
+        $sql = "INSERT INTO $table1 ($colum1) VALUES ($values1);";
+        $insert1 = $this->conn->query($sql);
+        if ($insert1 == true){
+            echo " thành công";
+        } else {
+            echo " k thành công".$this->conn->error;
+        }   
+    }
+    function select1($table1){
+        $sql = "SELECT * FROM $table1";
+        $select1 = $this->conn->query($sql);
+       $rows =  mysqli_fetch_all($select1);
+        if ($select1 == true){
                 echo " thành công";
             } else {
                 echo " k thành công".$this->conn->error;
-            }   
+            } 
+            return $rows;  
     }
-
-    function select($table){
-        $sql = "SELECT * FROM $table";
-        $select = $this->conn->query($sql);
-        $select = $this->conn->query($sql);
-        if ($select == true){
-                echo " thành công";
-            } else {
-                echo " k thành công".$this->conn->error;
-            }   
-    }
-
-    function update($table,$colum1,$values1){
-        $sql = "UPDATE $table SET $colum1 = $values1;";
-        $update = $this->conn->query($sql);
-        if ($update === TRUE){
-        echo " thành công";
-        } else {
-        echo " k thành công". $this->conn->error;
-        }   
-    }
-    function delete($table,$where){
-    $sql = "DELETE FROM $table WHERE $where";
-    $delete = $this->conn->query($sql);
-        if ($delete === TRUE){
-        echo " thành công";
-        } else {
-        echo " k thành công". $this->conn->error;
-        }   
-    }
+    
+    // function update($table,$colum1,$values1){
+    //     $sql = "UPDATE $table SET $colum1 = $values1;";
+    //     $update = $this->conn->query($sql);
+    //     if ($update === TRUE){
+    //     echo " thành công";
+    //     } else {
+    //     echo " k thành công". $this->conn->error;
+    //     }   
+    // }
+    // function delete($table,$where){
+    // $sql = "DELETE FROM $table WHERE $where";
+    // $delete = $this->conn->query($sql);
+    //     if ($delete === TRUE){
+    //     echo " thành công";
+    //     } else {
+    //     echo " k thành công". $this->conn->error;
+    //     }   
+    // }
 }      
     $db = new dbconnect();
     $db->connect();
@@ -67,17 +89,31 @@ class dbconnect {
     $values = "'2014-2108','cdxdctdt','nd'";
     $db->insert($table,$colum,$values);
 
-    $table = 'education';
-    $colum1 = "time";
-    $values1 = "'2017'";
-    $db->update($table,$colum1,$values1);
+    $table1 = 'skills';
+    $colum1 = "title,width";
+    $values1 = "'PHP','80%'";
+    $db->insert1($table1,$colum1,$values1);
 
-    $table = 'education';
-    $where = 'id=1';
-    $db->delete($table,$where);
+    $rows = $db->select1($table1,$table1);
+    var_dump($rows);
 
-    $table = 'education';
-    $db->select($table);
+    // $table = 'education';
+    // $colum1 = "time";
+    // $values1 = "'2017'";
+    // $db->update($table,$colum1,$values1);
+
+    // $table = 'education';
+    // $where = 'id=1';
+    // $db->delete($table,$where);
+
+    // $rows = $db->select($table);
+    // var_dump($rows);
+    // foreach($rows as $row){
+    //     var_dump($row);
+    //     echo "<br>";
+    // }
+
+
 ?>
 
 
